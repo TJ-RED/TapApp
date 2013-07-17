97930f605b37d3c7fd4ab51c5a9b7b61e68fada5
TJ-RED <tjred.apps@gmail.com>
2013-07-17 17:32:52 -0600

    Probando el drag and drop

---
+++ js/CodeBehind.js
@@ -0,0 +1,34 @@
+function onLoad() 
+{
+	
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// device APIs are available
+//
+function onDeviceReady() 
+{
+    // Register the event listener
+    navigator.splashscreen.show();
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+}
+
+// Handle the menu button
+//
+function onMenuKeyDown() 
+{
+	window.location.assign("#W_Menu");
+}
+
+function onPlaySeq()
+{
+	alert("Hola...");
+}
+
+
+function AddItem(id)
+{
+	var ListSender = document.getElementById(id);
+	
+	ListSender.innerHTML += "<li data-theme='c' class='ui-li ui-li-static ui-btn-up-c'>3</li>";
+}
